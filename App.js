bfca33076ab81407bd1b1b20da16402ac19fc5a0
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-03-08 18:00:04 +0000

    Adicionadas configurações básicas de tema de cor

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { ThemeContextProvider } from './src/contexts/ThemeContext';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeContextProvider>
+      <Home />
+    </ThemeContextProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/Home.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components/native'
+
+import {Text, Button} from 'react-native';
+
+import { useTheme } from '../hooks/useTheme';
+
+const Container = styled.View`
+    background-color: ${props => props.theme.background};
+    flex: 1;
+`
+const Title = styled.Text`
+    color: ${props => props.theme.title}
+`
+
+export function Home(){
+    const {toggleTheme} = useTheme()
+    return (
+        <Container>
+            <Title>Bem vindo(a)</Title>
+            <Button onPress={toggleTheme} title='Alternar tema' />
+        </Container>
+    )
+}
