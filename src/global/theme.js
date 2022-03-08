187b2b802508be6f9fcc3b71bcf0f49b46ce22a6
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-03-08 17:26:14 +0000

    Criado arquivo com as cores dos temas claro e escuro

---
+++ src/global/theme.js
@@ -0,0 +1,26 @@
+export const themes = {
+    light: {
+        primary: '#1EEC58',
+        error: '#FF5959',
+        background: '#FFFFFF',
+        title: '#404040',
+        subtitle: '#585858',
+
+        secondary1:'#696969', 
+        secondary2:'#9D9D9D', 
+        secondary3:'#F0F0F0', 
+        secondary4:'#FAFAFA', 
+    },
+    dark: {
+        primary: '#1EEC58',
+        error: '#FF5959',
+        background: '#161722',
+        title: '#EBEBEB',
+        subtitle: '#C1C1C1',
+
+        secondary1:'#CCCCCC', 
+        secondary2:'#8E8E8E', 
+        secondary3:'#303030', 
+        secondary4:'rgba(242, 242, 242, .1)', 
+    },
+}
