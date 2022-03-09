28806b5b4044bda28bcf1346c0977b595c5c857c
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-03-09 18:40:25 +0000

    Adicionadas rotas de gerenciamento de plantas e de vídeos

--- src/routes/BottomTabRoutes.jsx
+++ src/routes/BottomTabRoutes.jsx
@@ -1,6 +1,11 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+
+import {MaterialCommunityIcons as Icon} from '@expo/vector-icons'
+
 import { Config } from '../screens/Config'
 import { Home } from '../screens/Home'
+import { Videos } from '../screens/Videos'
+import { PlantsManagementRoutes } from './PlantsManagementRoutes'
 
 const Nav = createBottomTabNavigator()
 
@@ -9,6 +14,8 @@ export function BottomTabRoutes(){
         <Nav.Navigator screenOptions={{headerShown: false}}>
             <Nav.Screen name="home" component={Home} />
             <Nav.Screen name="config" component={Config} />
+            <Nav.Screen name="plantsManagement" component={PlantsManagementRoutes} options={{title: 'Plantas'}} />
+            <Nav.Screen name="videos" component={Videos} options={{title: 'Vídeos' }} />
         </Nav.Navigator>
     )
 }
---
+++ src/screens/Plants/index.jsx
@@ -0,0 +1,9 @@
+import {View, Text} from 'react-native';
+
+export function Plants(){
+    return (
+        <View>
+            <Text>Suas plantas</Text>
+        </View>
+    )
+}
