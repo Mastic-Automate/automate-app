08ee0cd19add0b6290a5e12796140d3cbab9024d
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-04-09 17:06:39 +0000

    Criada tela de cadastro

--- src/screens/Register/index.jsx
+++ src/screens/Register/index.jsx
@@ -1,12 +1,76 @@
-import {View, Button, Text} from 'react-native'
+import styled from 'styled-components/native'
+import {View, Text} from 'react-native'
+
+import {MaterialCommunityIcons, MaterialIcons} from '@expo/vector-icons'
+
+import {Input} from '../../components/Input'
+import {Button} from '../../components/Button'
+
+const Container = styled.SafeAreaView`
+    padding:10px;
+    background-color: ${props => props.theme.background};
+    align-items:center;
+    justify-content: space-around;
+    flex:1;
+`
+const Title = styled.Text`
+    font-size:30px;
+    color: ${props => props.theme.title};
+`
+
+const Inputs = styled.View`
+    width:100%;
+`
+
+const BottomInfo = styled.View`
+    align-items: center;
+`
+const BottomText = styled.Text`
+    font-size: 20px;
+    color: ${props => props.theme.secondary1};
+`
+
+const LoginLink = styled.TouchableOpacity`
+    color: ${props => props.theme.primary};
+    font-size: 20px;
+    font-weight: bold;
+    font-family: Poppins;
+`
 
 export function Register({navigation}){
 
     return (
-        <View>
-            <Text>Se registre aqui</Text>
-            <Text>Já possui uma conta?</Text>
-            <Button onPress={()=> navigation.replace('login')} title="Login" />
-        </View>
+        <Container>
+            <Title>Cadastrar</Title>
+            <Inputs>
+                <Input 
+                    iconName="account-circle-outline"
+                    iconType={MaterialCommunityIcons}
+                    style={{marginTop: 10}}
+                    placeholder="Email"
+                />
+                <Input 
+                    iconName="lock-outline"
+                    iconType={MaterialIcons}
+                    style={{marginTop: 10}}
+                    placeholder="Senha"
+                />
+                <Input 
+                    iconName="lock-outline"
+                    iconType={MaterialIcons}
+                    style={{marginTop: 10}}
+                    placeholder="Confirmar senha"
+                />
+                <Button
+                    text="Cadastrar"
+                    style={{marginTop:10}}
+                />
+
+            </Inputs>
+            <BottomInfo>
+                <BottomText>Já possui uma conta?</BottomText>
+                <LoginLink onPress={() => navigation.replace('login')}>Login</LoginLink>
+            </BottomInfo>
+        </Container>
     )
 }
