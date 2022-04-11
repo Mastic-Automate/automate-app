7defef5287b0013bbf87de1898e28a74d8a3aaa6
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-04-11 12:15:20 +0000

    Tela de adição de planta estruturada

--- src/routes/PlantsManagementRoutes.jsx
+++ src/routes/PlantsManagementRoutes.jsx
@@ -3,6 +3,7 @@ import { Plants } from '../screens/Plants'
 import { EditPlant } from '../screens/EditPlant'
 import { InfoPlant } from '../screens/InfoPlant'
 import {BluetoothConnection} from '../screens/BluetoothConnection'
+import {AddPlant} from '../screens/AddPlant'
 
 const Nav = createNativeStackNavigator()
 
@@ -11,6 +12,7 @@ export function PlantsManagementRoutes(){
         <Nav.Navigator screenOptions={{headerShown: false}}>
             <Nav.Screen name="plants" component={Plants} />
             <Nav.Screen name="bluetooth-connection" component={BluetoothConnection} />
+            <Nav.Screen name="add-plant" component={AddPlant} />
             <Nav.Screen name="edit-plant" component={EditPlant} />
             <Nav.Screen name="info-plant" component={InfoPlant} />
         </Nav.Navigator>
