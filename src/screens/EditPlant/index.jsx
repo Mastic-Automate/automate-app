1daeff7a65697fb4bcd8ea9e9611dc4696761d9d
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-04-11 11:04:36 +0000

    Tela de editar planta estruturada

--- src/screens/EditPlant/index.jsx
+++ src/screens/EditPlant/index.jsx
@@ -1,14 +1,27 @@
 import styled from 'styled-components/native'
 
+import {Text} from 'react-native'
+
+import {Button} from '../../components/Button'
+
 const Container = styled.View`
     padding: 10px;
     align-items:center;
     justify-content:space-around;
+    background-color:${props => props.theme.background};
+    flex:1;
 `
 const Title = styled.Text`
     color: ${props => props.theme.title};
     font-size: 30px;
+`
 
+const InputsContainer = styled.View`
+`
+const BottomButtonsContainer = styled.View`
+    flex-direction:row;
+    justify-content:center ;
+    width:100%;
 `
 
 function EditPlant({route}){
@@ -16,7 +29,15 @@ function EditPlant({route}){
     return (
         <Container>
             <Title>Editar planta</Title>
-
+            <InputsContainer>
+                <Text>Input 1</Text>
+                <Text>Input 2</Text>
+                <Text>Input 3</Text>
+            </InputsContainer>
+            <BottomButtonsContainer>
+                <Button text="Salvar" style={{flex:1, margin:5}} />
+                <Button text="Cancelar" outline style={{flex:1, margin:5}} />
+            </BottomButtonsContainer>
         </Container>
     )
 }
