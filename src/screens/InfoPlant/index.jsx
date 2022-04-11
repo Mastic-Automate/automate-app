ff956d3abd2e740913b3511820a44f32cd924952
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-04-11 11:09:41 +0000

    Tela de informações da planta estruturada

--- src/screens/InfoPlant/index.jsx
+++ src/screens/InfoPlant/index.jsx
@@ -1,20 +1,33 @@
+import { Text } from 'react-native'
 import styled from 'styled-components/native'
+import { Button } from '../../components/Button'
 
 const Container = styled.View`
     flex:1;
-    align-items:center;
     justify-content:space-around;
+    background-color:${props => props.theme.background};
+    padding:10px;
 `
 
 const Title = styled.Text`
     font-size: 30px;
     color: ${props => props.theme.title};
+    width:100%;
+    text-align:center;
+`
+const InfoTilesContainer = styled.View`
+    flex:1;
+    width:100%;
 `
 
 function InfoPlant(){
     return (
         <Container>
             <Title>Informações da planta</Title>
+            <InfoTilesContainer>
+                <Text>Informação</Text>
+            </InfoTilesContainer>
+            <Button text="Salvar relatório na nuvem" />
         </Container>
     )
 }
