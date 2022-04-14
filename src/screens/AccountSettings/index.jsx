dd37b95539c45f5deb02d7d0c3ab4e882462f552
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-04-14 13:44:23 +0000

    WIP tela de configurações de conta

--- src/screens/AccountSettings/index.jsx
+++ src/screens/AccountSettings/index.jsx
@@ -1,10 +1,29 @@
-import {View, Text} from 'react-native';
+import styled from 'styled-components/native'
+
+import {Button} from '../../components/Button'
+
+const Container = styled.View`
+    padding-left: 10px;
+    padding-right: 10px;
+    background-color:${props => props.theme.background};
+    flex:1;
+`
+
+const Title = styled.Text`
+    color: ${props => props.theme.title};
+    font-size: 30px;
+    width: 100%;
+    text-align:center;
+    margin-top: 75px;
+`
 
 function AccountSettings(){
     return (
-        <View>
-            <Text>Configurações de conta</Text>
-        </View>
+        <Container>
+            <Title>Configurações de conta</Title>
+
+            <Button text="Logout" />
+        </Container>
     )
 }
 
