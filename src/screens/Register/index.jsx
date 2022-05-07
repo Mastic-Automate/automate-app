b6621c014ac0c3510674275cf2f910e6f236a312
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-05-07 16:52:05 +0000

    Adicionada verificação de inputs no formulário de cadastro

--- src/screens/Register/index.jsx
+++ src/screens/Register/index.jsx
@@ -1,12 +1,22 @@
 import styled from 'styled-components/native'
-import {View, Text} from 'react-native'
 
 import {MaterialCommunityIcons, MaterialIcons} from '@expo/vector-icons'
 
-import {Input} from '../../components/Input'
+import {useForm} from 'react-hook-form'
+
+import {FormInput as Input} from '../../components/FormInput'
 import {Button} from '../../components/Button'
 
-const Container = styled.SafeAreaView`
+import * as yup from 'yup'
+import {yupResolver} from '@hookform/resolvers/yup'
+
+const schema = yup.object({
+    email: yup.string().required("Campo email é obrigatório").email("Email inválido"),
+    password: yup.string().required("Campo senha obrigatório").min(6, "Senha deve ter ao menos 6 caracteres"),
+    'password-confirm': yup.string().required("Campo de confirmação de senha obrigatório").oneOf([yup.ref('password'), null], "Senha de confirmação deve ser igual a primeira senha")
+})
+
+const Container = styled.KeyboardAvoidingView`
     padding:10px;
     background-color: ${props => props.theme.background};
     align-items:center;
@@ -38,32 +48,51 @@ const LoginLink = styled.TouchableOpacity`
 `
 
 export function Register({navigation}){
+    const {control, handleSubmit, formState:{errors}} = useForm({
+        resolver:yupResolver(schema)
+    })
 
+    function handleUserRegister(data){
+        console.log(data)
+    }
     return (
         <Container>
             <Title>Cadastrar</Title>
             <Inputs>
-                <Input 
+                <Input
+                    name="email"
+                    control={control}
                     iconName="account-circle-outline"
                     iconType={MaterialCommunityIcons}
                     style={{marginTop: 10}}
                     placeholder="Email"
+                    error={errors.email}
+                    keyBoardType="email"
                 />
                 <Input 
+                    name="password"
+                    control={control}
                     iconName="lock-outline"
                     iconType={MaterialIcons}
                     style={{marginTop: 10}}
                     placeholder="Senha"
+                    error={errors.password}
+                    secureTextEntry
                 />
-                <Input 
+                <Input
+                    name="password-confirm"
+                    control={control}
                     iconName="lock-outline"
                     iconType={MaterialIcons}
                     style={{marginTop: 10}}
                     placeholder="Confirmar senha"
+                    error={errors['password-confirm']}
+                    secureTextEntry
                 />
                 <Button
                     text="Cadastrar"
                     style={{marginTop:10}}
+                    onPress={handleSubmit(handleUserRegister)}
                 />
 
             </Inputs>
