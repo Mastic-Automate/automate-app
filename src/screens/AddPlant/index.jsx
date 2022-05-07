c79fecd2903d935fa2f07866c394a0ab9b579ab7
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-05-07 17:52:05 +0000

    Adicionada verificação de input na tela de adição de planta

--- src/screens/AddPlant/index.jsx
+++ src/screens/AddPlant/index.jsx
@@ -1,21 +1,48 @@
 import { Button } from '../../components/Button'
 
-import {Input} from '../../components/Input'
+import {FormInput as Input} from '../../components/FormInput'
+
+import {useForm} from 'react-hook-form'
+import * as yup from 'yup'
+import {yupResolver} from '@hookform/resolvers/yup'
 
 import {BottomButtonsContainer, Container, InputLabel, InputsContainer, Title} from './styles'
 
+const schema = yup.object({
+    name: yup.string().required("Nome é obrigatório"),
+    model: yup.string().required("Modelo é obrigatório")
+})
+
 function AddPlant(){
+    const {handleSubmit, control, formState:{errors}} = useForm({
+        resolver: yupResolver(schema)
+    })
+
+    function handleAddPlant(data){
+        console.log(data)
+    }
+    
     return (
         <Container>
             <Title>Adicionar planta</Title>
             <InputsContainer>
                 <InputLabel>Nome</InputLabel>
-                <Input placeholder="Tomates incríveis"  />
+                <Input 
+                    placeholder="Tomates incríveis"
+                    control={control}
+                    name="name"  
+                    error={errors.name}
+                />
                 <InputLabel>Modelo</InputLabel>
-                <Input placeholder="Tomate"  />
+                <Input 
+                    placeholder="Tomate"
+                    control={control}
+                    name="model"
+                    error={errors.model}
+                />
             </InputsContainer>
             <BottomButtonsContainer>
-                <Button text="Salvar" style={{flex:1, margin:5}} />
+                <Button text="Salvar" style={{flex:1, margin:5}} onPress={handleSubmit(handleAddPlant)} />
                 <Button text="Cancelar" style={{flex:1, margin:5}} outline />
             </BottomButtonsContainer>
         </Container>
