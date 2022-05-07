9f3d355d3dfbc8a83843aef729c9d2aa50e53f09
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-05-07 18:07:37 +0000

    Adicionada verificação de inputs na tela de edição de planta

--- src/screens/EditPlant/index.jsx
+++ src/screens/EditPlant/index.jsx
@@ -1,10 +1,26 @@
 import { Button } from '../../components/Button'
-import { Input } from '../../components/Input'
+import {FormInput as Input } from '../../components/FormInput'
 
 import {BottomButtonsContainer, Container, InputsContainer, PlantInfoLabel, Title} from './styles'
 
+import {useForm} from 'react-hook-form'
+import * as yup from 'yup'
+import {yupResolver} from '@hookform/resolvers/yup'
+
+const schema = yup.object({
+    name: yup.string().required("Nome é obrigatório"),
+    model: yup.string().required("Modelo é obrigatório")
+})
+
 function EditPlant({ route }) {
     const { id } = route.params
+    const {control, handleSubmit, formState:{errors}} = useForm({
+        resolver:yupResolver(schema)
+    })
+
+    function handleSaveEdit(data){
+        console.log(data)
+    }
     return (
         <Container>
             <Title>Editar planta</Title>
@@ -13,17 +29,23 @@ function EditPlant({ route }) {
                 <Input
                     placeholder="Nome da planta"
                     value="Meus tomates"
+                    control={control}
+                    name="name"
+                    error={errors.name}
                 />
                 <PlantInfoLabel>Modelo</PlantInfoLabel>
                 <Input
                     placeholder="Modelo da planta"
                     value="Tomate"
+                    control={control}
+                    name="model"
+                    error={errors.model}
                 />
                 <Button text="Remover planta" style={{ marginTop: 5 }} negative />
 
             </InputsContainer>
             <BottomButtonsContainer>
-                <Button text="Salvar" style={{ flex: 1, margin: 5 }} />
+                <Button text="Salvar" onPress={handleSubmit(handleSaveEdit)} style={{ flex: 1, margin: 5 }} />
                 <Button text="Cancelar" outline style={{ flex: 1, margin: 5 }} />
             </BottomButtonsContainer>
         </Container>
