b7a6ccec803690ece346599fde45808b9605d85a
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-05-16 07:58:56 -0300

    Adicionada configuração de variáveis de ambiente

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,15 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'react-native-reanimated/plugin'
+      'react-native-reanimated/plugin',
+      ['module:react-native-dotenv', {
+        'moduleName':'@env',
+        'path':'.env',
+        'blacklist':null,
+        'whitelist':null,
+        'safe':false,
+        'allowUndefined':true
+      }]
     ],
   };
 };
