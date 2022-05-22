c1484a812652cca9e80b44d1dd323ae337c41564
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-05-22 23:22:40 +0000

    Corrigidos erros que impossibilitavam renderização da tela de plantas

--- src/components/PlantTile/index.jsx
+++ src/components/PlantTile/index.jsx
@@ -1,8 +1,8 @@
-import styled, {useTheme} from 'styled-components/native';
+import styled, { useTheme } from 'styled-components/native';
 
-import {MaterialIcons} from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
 
-import {useNavigation} from '@react-navigation/native'
+import { useNavigation } from '@react-navigation/native'
 
 const Container = styled.View`
     flex-flow: row nowrap;
@@ -17,7 +17,7 @@ const Container = styled.View`
     justify-content: space-between;
 `;
 
-const Title = styled.View`
+const Title = styled.Text`
     font-size: 20px;
     color: ${props => props.theme.secondary1};
     font-family: Poppins;
@@ -27,23 +27,23 @@ const IconsWrapper = styled.View`
     flex-direction:row;
 `;
 
-export default function PlantTile({id, text}) {
+export default function PlantTile({ id, text }) {
     const navigation = useNavigation()
-    const {secondary1} = useTheme()
+    const { secondary1 } = useTheme()
 
-    function openPlantInfo(){
-        navigation.replace('bluetooth-connection', 
+    function openPlantInfo() {
+        navigation.replace('bluetooth-connection',
             {
-                target:'info-plant',
-                params:{id:id}
+                target: 'info-plant',
+                params: { id: id }
             }
         )
     }
-    function openPlantEdit(){
-        navigation.replace('bluetooth-connection', 
+    function openPlantEdit() {
+        navigation.replace('bluetooth-connection',
             {
-                target:'edit-plant',
-                params:{id:id}
+                target: 'edit-plant',
+                params: { id: id }
             }
         )
     }
@@ -52,12 +52,12 @@ export default function PlantTile({id, text}) {
         <Container>
             <Title>{text}</Title>
             <IconsWrapper>
-                <MaterialIcons name='info-outline' size={40} color={secondary1} style={{margin:3}}  onPress={openPlantInfo}/>
-                <MaterialIcons name="edit" size={40} color={secondary1} style={{margin:3}} onPress={openPlantEdit} />
+                <MaterialIcons name='info-outline' size={40} color={secondary1} style={{ margin: 3 }} onPress={openPlantInfo} />
+                <MaterialIcons name="edit" size={40} color={secondary1} style={{ margin: 3 }} onPress={openPlantEdit} />
             </IconsWrapper>
         </Container>
     )
-    
+
 }
 
 
