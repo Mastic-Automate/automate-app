d11c3cb4e93c666c68bea824b79928b1d7df3271
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-05-22 22:40:23 +0000

    Melhorada interação com o input através do teclado

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,32 +1,28 @@
-import {createContext, useState} from 'react'
+import { createContext, useState } from 'react'
 
-import {api} from '../services/api'
+import { api } from '../services/api'
 
 const AuthContext = createContext({})
 
-function AuthContextProvider({children}){
+function AuthContextProvider({ children }) {
     const [user, setUser] = useState(null)
-    async function signIn(userEmail, userPassword){
-        const requestBody = {userEmail, userPassword}
+    async function signIn(userEmail, userPassword) {
+        const requestBody = { userEmail, userPassword }
         try {
-            const response = await api.post('/signin', requestBody, {
-                headers: {
-                    'Access-Control-Allow-Origin':'*'
-                }
-            })
-            console.log(response)
-        } catch(err) {
+            const response = await api.post('/signin', requestBody)
+            console.log(response.data)
+        } catch (err) {
             console.log(err)
         }
     }
-    function signUp(userEmail, userName, userPassword){
+    function signUp(userEmail, userName, userPassword) {
         api.post('/signin')
-    }   
+    }
     return (
-        <AuthContext.Provider value={{user, signIn, signUp}}>
+        <AuthContext.Provider value={{ user, signIn, signUp }}>
             {children}
         </AuthContext.Provider>
     )
 }
 
-export {AuthContextProvider, AuthContext}
+export { AuthContextProvider, AuthContext }
--- src/screens/Login/index.jsx
+++ src/screens/Login/index.jsx
@@ -1,42 +1,45 @@
-import {MaterialCommunityIcons as MaterialCommunity, MaterialIcons as Material} from '@expo/vector-icons'
+import { MaterialCommunityIcons as MaterialCommunity, MaterialIcons as Material } from '@expo/vector-icons'
 
-import {Button} from '../../components/Button'
-import {FormInput as Input } from '../../components/FormInput'
+import { Button } from '../../components/Button'
+import { FormInput as Input } from '../../components/FormInput'
 
-import {useAuth} from '../../hooks/useAuth'
+import { useAuth } from '../../hooks/useAuth'
 
-import {BottomLink, BottomLinkText, BottomText, BottomView, Container, InputsView, Title} from './styles'
+import { BottomLink, BottomLinkText, BottomText, BottomView, Container, InputsView, Title } from './styles'
 
-import {useForm} from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 import * as yup from 'yup'
-import {yupResolver} from '@hookform/resolvers/yup'
+import { yupResolver } from '@hookform/resolvers/yup'
 
 const schema = yup.object({
     email: yup.string().required("Campo de email obrigatório").email("Email inválido"),
     password: yup.string().required("Campo de senha obrigatório")
 })
 
-export function Login({navigation}){
-    const {signIn:authSignIn} = useAuth()
-    const {control, handleSubmit, formState:{errors}} = useForm({
+export function Login({ navigation }) {
+    const { signIn: authSignIn } = useAuth()
+    const { control, handleSubmit, formState: { errors } } = useForm({
         resolver: yupResolver(schema)
     })
-    function signIn(data){
+    function signIn(data) {
         authSignIn(data.email, data.password)
     }
     return (
         <Container>
             <Title>Login</Title>
             <InputsView>
-            
+
                 <Input
                     name="email"
                     control={control}
                     error={errors.email}
+                    keyboardType="email-address"
                     placeholder="Email"
                     iconType={MaterialCommunity}
                     iconName="account-circle-outline"
-                    style={{marginTop:10}}
+                    style={{ marginTop: 10 }}
+                    autoCorrect={false}
+                    autoCapitalize="none"
                 />
                 <Input
                     name="password"
@@ -46,14 +49,16 @@ export function Login({navigation}){
                     iconType={Material}
                     iconName="lock-outline"
                     secureTextEntry
-                    style={{marginTop:10}}
+                    style={{ marginTop: 10 }}
+                    autoCorrect={false}
+                    autoCapitalize="none"
                 />
 
-                <Button text="Login" onPress={handleSubmit(signIn)} style={{marginTop:10}} />
+                <Button text="Login" onPress={handleSubmit(signIn)} style={{ marginTop: 10 }} />
             </InputsView>
             <BottomView>
                 <BottomText>Ainda não possui uma conta?</BottomText>
-                <BottomLink onPress={()=> navigation.replace('register')}>
+                <BottomLink onPress={() => navigation.replace('register')}>
                     <BottomLinkText>Registre-se! </BottomLinkText>
                 </BottomLink>
             </BottomView>
