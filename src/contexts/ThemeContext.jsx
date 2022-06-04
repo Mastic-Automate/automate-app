7f63f3cf24c5792a3b63ec81b2bce7e670417616
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-06-04 21:14:23 +0000

    Adicionada persistência de tema de cores

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -1,24 +1,45 @@
-import { useMemo, useState, createContext } from 'react'
+import { useMemo, useState, createContext, useEffect } from 'react'
 import {ThemeProvider} from 'styled-components/native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 import { themes } from '../global/theme'
 
 export const ThemeContext = createContext({})
 
 export function ThemeContextProvider({children}){
-    const [theme, setTheme] = useState('light')
+    const [theme, setTheme] = useState(null)
     const currentTheme = useMemo(() => themes[theme], [theme])
+
+    const [isThemeLoading, setIsThemeLoading] = useState(true)
 
     function toggleTheme(){
         setTheme(theme === 'light'? 'dark' : 'light')
     }
+
+    useEffect(()=>{
+        if(!!theme) {
+            AsyncStorage.setItem('@AppTheme', theme)
+        }
+    }, [theme])
+
+    useEffect(()=> {
+        AsyncStorage.getItem('@AppTheme').then(result => {
+            if(!!result){
+                return setTheme(result)
+            }
+            setTheme('light')
+            setIsThemeLoading(false)
+        })
+    }, [])
     
     return (
         <ThemeContext.Provider value={{toggleTheme, theme}}>
+            {!isThemeLoading && (
+                <ThemeProvider theme={currentTheme}>
+                    {children}
+                </ThemeProvider>
+            )}
 
-            <ThemeProvider theme={currentTheme}>
-                {children}
-            </ThemeProvider>
         </ThemeContext.Provider>
     )
 }
