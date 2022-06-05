883efed608685a1d6b94c576273e6e847d108832
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-06-05 22:31:55 +0000

    Adicionado botão de voltar para facilitar a navegação

--- src/routes/ConfigScreensRoutes.jsx
+++ src/routes/ConfigScreensRoutes.jsx
@@ -5,9 +5,12 @@ const stack = createNativeStackNavigator()
 import {Config} from '../screens/Config'
 import {AccountSettings} from '../screens/AccountSettings'
 
+import {useTheme} from 'styled-components/native'
+
 function ConfigScreensRoutes(){
+    const {primary} = useTheme()
     return (
-        <stack.Navigator screenOptions={{headerShown:false}}>
+        <stack.Navigator screenOptions={{headerShown:true, headerTransparent:true, headerTitle:'', headerTintColor:primary}}>
             <stack.Screen name="config-home" component={Config} />
             <stack.Screen name="account" component={AccountSettings} />
         </stack.Navigator>
