93172e60d08ac884605f75e41ffa7f3fd2a862ed
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-06-26 16:33:17 +0000

    Removido código não utilizado na tela bluetooth

--- src/screens/BluetoothConnection/index.jsx
+++ src/screens/BluetoothConnection/index.jsx
@@ -59,148 +59,7 @@ const NewSubtitle = styled.Text`
 
 `
 
-
-const AutomateName = styled.Text`
-width: 18.50rem;
-height: 3.94;
-margin-left: 4.438rem;
-margin-top: 1.063rem;
-
-
-font-family: 'ProximaNovaBold';
-font-size: 2rem;
-
-
-color: #000000;
-
-
-
-`
-
-const NavDiv = styled.View`
-    width: 100%;
-    height: 3.75rem;
-    flex-direction: row;
-    align-items: center;
-    
-    margin-top: 1.875rem;
-`
-const LineDiv = styled.View`
-    width: 90%;
-    height: 0.19rem;
-    background-color: #000718;
-    margin-top: 1.1rem;
-   align-self: center;
-`
-// Alça em inglês é muito dificio, então vai barrinha mesmo
-const Barrinha = styled.View`
-
-    width: 25%;
-    //width: 7rem;
-    height: 0.63rem;
-    background-color: #8FA9BB;
-    align-self: center;
-    border-radius: 0.938rem;
-    margin-top: 0.875rem;
-    
-`
-
-const ContainerMenuFooter = styled.View`
-    width: 100%;
-    height: 8.5rem;
-    background-color: #D7E1E8;
-    //display: block;
-    //align-items: flex-start;
-    border-top-left-radius: 3.375rem;
-    border-top-right-radius: 3.375rem;
-
-    
-`
-
-const ContentMenuFooter = styled.View`
-    width: 90%;
-    height: 25%;
-    background-color: #265374;
-
-    //align-items: flex-start;
-    margin-top: 13%;
-    align-content: center;
-    align-items: center;
-    align-self: center;
-    justify-content: space-around;
-    flex: 0;
-    flex-direction: row;
-    
-`
-
-const BatteryIcon = styled(MaterialIcons)`
-    font-size: 4.688rem;
-    transform: rotate(90deg);
-    //align-self: flex-start;
-   // height: min-content;
-    //margin-left: 3.25rem;
-   
-    //display: inline-flex;
-    position: static;
-    display: inline;
-`
-
-const DropIcon = styled(MaterialIcons)`
-    font-size: 4.688rem;
-   // align-self: center;
-   // display: inline-block;
-
-   // flex-direction: column;
-    //display: inline-flex;
-    position: static;
-    display: inline;
-`
-
-const SunIcon = styled(MaterialIcons)`
-    font-size: 4.688rem;
-   // align-self: center;
-   // display: inline-block;
-
-   // flex-direction: column;
-    //display: inline-flex;
-    position: static;
-    display: inline;
-`
-
-const DescText = styled(Text)`
-    position: absolute;
-    font-size: 1.2rem;
-    align-self: center;
-    top: 75%;
-    text-align: center;
-`
-
-function ScanAutomate(){
-    return (
-        <>
-            <NewTitle >Procurando dispositivo</NewTitle>
-            <NewSubtitle>Seu dispositivo Automate deve aparecer aqui, em breve.</NewSubtitle>
-            {/* <LineDiv /> */}
-            <Image
-                style={{width: '26.75rem', height: '20.06rem', marginTop: '2.25rem',  }}
-                source={require('../../../assets/conexão.gif')} 
-            />
-
-            {/* <ContainerMenuFooter style={{marginTop: '10.188rem'}}>
-                <BatteryIcon name='battery-std' color={automateFound?"#42db49":'#4e4e4e'} />
-                <DropIcon name='opacity' color={automateFound?"#006eff":'#4e4e4e'} />
-                <SunIcon name='brightness-7' color={automateFound?"#e9db19":'#4e4e4e'} />
-            </ContainerMenuFooter> */}
-            {/*N sei praq que isso tá servindo, ent comentei*/}
-        </>
-    )
-    
-}
-
 function BluetoothConnection({navigation, route}){
-   
-    const [automateFound, setAutomateFound] = useState(true);
-
     const {target, params} = route.params
     useEffect(()=>{
         setTimeout(function (){
@@ -212,16 +71,16 @@ function BluetoothConnection({navigation, route}){
         'ProximaNovaBold': require('../../../assets/fonts/proximaNova/ProximaNovaBold.otf'),
         'ProximaNovaExtraBold': require('../../../assets/fonts/proximaNova/ProximaNovaExtraBold.otf'),
         'Montserrat': require('../../../assets/fonts/Montserrat/Montserrat-Regular.ttf'),
-      });
-
-    
+    });
 
     return (
         <Container>
-            <NavDiv>
-                <MaterialIcons style={{fontSize: '1.50rem', fontWeight: '900', marginLeft: '2rem'}} name='arrow-back-ios' />
-            </NavDiv>
-            <ScanAutomate />
+            <NewTitle >Procurando dispositivo</NewTitle>
+            <NewSubtitle>Seu dispositivo Automate deve aparecer aqui, em breve.</NewSubtitle>
+            <Image
+                style={{width: '26.75rem', height: '20.06rem', marginTop: '2.25rem',  }}
+                source={require('../../../assets/conexão.gif')} 
+            />
                 
         </Container>
     )
