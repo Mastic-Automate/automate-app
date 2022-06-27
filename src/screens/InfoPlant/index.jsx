907941e8c0eca5b03725a21ea4b0d5ec57218a2f
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-06-27 17:12:40 +0000

    Iniciada repaginação da tela de infos

--- src/screens/InfoPlant/index.jsx
+++ src/screens/InfoPlant/index.jsx
@@ -1,39 +1,87 @@
 import { Text } from 'react-native'
 import styled from 'styled-components/native'
 
+import { Feather, Ionicons, FontAwesome5 } from '@expo/vector-icons';
+
 import { Button } from '../../components/Button'
-import {InfoPlantTile} from '../../components/InfoPlantTile'
+import { useTheme } from 'styled-components';
 
 const Container = styled.View`
     flex:1;
     justify-content:space-around;
-    background-color:${props => props.theme.background};
-    padding:10px;
+    background-color:${props => props.theme.background1};
 `
 
 const Title = styled.Text`
-    font-size: 30px;
+    font-size: 48px;
     color: ${props => props.theme.title};
-    width:100%;
-    text-align:center;
+    font-weight: bold;
 `
-const InfoTilesContainer = styled.View`
+
+const HeaderSection = styled.View`
+    width: 100%;
+    padding-left: 20px;
+    flex-direction: row;
+    align-items:center;
+`
+const MidSection = styled.View`
     flex:1;
-    width:100%;
-    margin-top: 30px;
+    flex-direction:row;
+    margin-top: 5px;
+`
+const MidSectionCol1 = styled.View`
+    padding-left: 12px;
+`
+const InfoSquare = styled.View`
+    border: 4px solid ${props => props.theme.blue};
+    width: 125px;
+    height: 125px;
+    border-radius: 20px;
+    align-items:center;
+    margin-bottom: 20px;
+`
+const InfoSquareText = styled.Text`
+    color: ${props => props.theme.text1};
+`
+const BottomSection = styled.View`
+    width: 100%;
+    height: 200px;
+    justify-content:space-between ;
+`
+const BottomText = styled.Text`
+    color: ${props => props.theme.text2};
+    font-size: 20px;
 `
 
 function InfoPlant(){
+    const themeColors = useTheme()
     return (
         <Container>
-            <Title>Informações da planta</Title>
-            <InfoTilesContainer>
-                <InfoPlantTile 
-                    text="Umidade do solo"
-                    value="20%"
-                />
-            </InfoTilesContainer>
-            <Button text="Salvar relatório na nuvem" />
+            <HeaderSection>
+                <Title>Blueberry</Title>
+            </HeaderSection>
+            <MidSection>
+                <MidSectionCol1>
+                    <InfoSquare>
+                        <Feather name="sun" color={themeColors.blue} size={52} />
+                        <InfoSquareText>100%</InfoSquareText>
+                    </InfoSquare>
+                    <InfoSquare>
+                        <Ionicons name="water-outline" color={themeColors.blue} size={52} />
+                        <InfoSquareText>100%</InfoSquareText>
+                    </InfoSquare>
+                    <InfoSquare>
+                        <FontAwesome5 name="temperature-high" color={themeColors.blue} size={52} />
+                        <InfoSquareText>100%</InfoSquareText>
+                    </InfoSquare>
+                </MidSectionCol1>
+            </MidSection>
+            <BottomSection>
+                <BottomText>
+                    is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+                </BottomText>
+                <Button text="Salvar relatório na nuvem" />
+            </BottomSection>
         </Container>
     )
 }
