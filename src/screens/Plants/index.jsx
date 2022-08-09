b71d52e3c27ee2c2d276c11db7cd50e58cc4203e
Victor Lira <victor.liracarlos@gmail.com>
2022-08-09 21:08:49 +0000

    Atualizado design

--- src/screens/Plants/index.jsx
+++ src/screens/Plants/index.jsx
@@ -1,22 +1,27 @@
 import { useTheme } from 'styled-components/native';
 
-import { MaterialIcons } from '@expo/vector-icons';
+import { MaterialIcons, Feather, FontAwesome5 } from '@expo/vector-icons';
 
 import {
     Container, 
     Title,
     AddButton,
     MainContent,
+    TopSection,
+    PlantSection,
     SomePlantsContainer,
     UserPlantsContainer,
-    UserPlantsTitle
-} from './styles'
+    UserPlantsTitle,
+    InputsRow,
+    FilterButton,
+} from './styles';
+
+import {Input} from '../../components/Input';
 import { PlantCard } from './PlantCard';
-import {UserPlantCard} from './UserPlantCard'
+import {UserPlantCard} from './UserPlantCard';
 
-
-import {plants} from '../../global/plants' 
-import {appImages} from '../../global/images'
+import {plants} from '../../global/plants' ;
+import {appImages} from '../../global/images';
 
 export function Plants({ navigation }) {
     const { primary } = useTheme();
@@ -24,37 +29,62 @@ export function Plants({ navigation }) {
     return (
         <Container>
             <MainContent>
-                <Title>Todas as plantas</Title>
-                <SomePlantsContainer horizontal>
-                    {plants.map(plant => (
-                        <PlantCard 
-                            description={plant.description}
-                            id={plant.id}
-                            image={plant.image}
-                            name={plant.name}
-                            key={plant.id}
-                            style={{marginHorizontal:5}}
-                        />
-                    ))}
+                <TopSection>
+                    <Title>Todas as plantas</Title>
                     
-                </SomePlantsContainer>
-                <UserPlantsTitle horizontal>
-                    Suas plantas
-                </UserPlantsTitle>
-                <UserPlantsContainer horizontal>
-                    <UserPlantCard 
-                        image={appImages['plant_corner']}
-                        name="Suculenta"
-                        description="Sala"
-                        style={{marginLeft:10}}
-                    />
-                    <UserPlantCard 
-                        image={appImages['plant_corner']}
-                        name="Suculenta1"
-                        description="Telhado"
-                        style={{marginLeft:10}}
-                    />
-                </UserPlantsContainer>
+                </TopSection>
+                
+
+                <PlantSection>
+                    <InputsRow>
+                            <Input 
+                                iconType={Feather}
+                                iconName="search"
+                                style={{
+                                    flex:1
+                                }}
+                                placeholder="Pesquisar"
+                            />
+                            <FilterButton>
+                                <FontAwesome5 
+                                    color="#ffffff"
+                                    size={30}
+                                    name="filter"
+                                />
+                            </FilterButton>
+                        </InputsRow>
+                    <SomePlantsContainer horizontal>
+                        {plants.map(plant => (
+                            <PlantCard 
+                                description={plant.description}
+                                id={plant.id}
+                                image={plant.image}
+                                name={plant.name}
+                                key={plant.id}
+                                style={{marginHorizontal:5}}
+                            />
+                        ))}
+                        
+                    </SomePlantsContainer>
+                    <UserPlantsTitle horizontal>
+                        Suas plantas
+                    </UserPlantsTitle>
+                    <UserPlantsContainer horizontal>
+                        <UserPlantCard 
+                            image={appImages['plant_corner']}
+                            name="Suculenta"
+                            description="Sala"
+                            style={{marginLeft:10}}
+                        />
+                        <UserPlantCard 
+                            image={appImages['plant_corner']}
+                            name="Suculenta1"
+                            description="Telhado"
+                            style={{marginLeft:10}}
+                        />
+                    </UserPlantsContainer>
+                </PlantSection>
+                
             </MainContent>
             
             <AddButton onPress={() => navigation.navigate('bluetooth-connection', { target: 'add-plant', params: {} })}>
