8ad0e127dd1279796794a4147ed188191987ab7e
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-08-19 16:20:07 +0000

    Adicionadas imagens para as plantas até então inseridas

--- src/global/images.js
+++ src/global/images.js
@@ -2,10 +2,16 @@ export const appImages = {
     'plant_corner':require('../assets/plant_corner.png'),
     'profile_placeholder':require('../assets/profile_placeholder.png'),
     'plant1':require('../assets/plant1.png'),
-    'morango':require('../assets/morango.png'),
+    'plant2':require('../assets/planta2.png'),
     'alho':require('../assets/alho.png'),
     'pimenta_do_reino':require('../assets/pimenta_do_reino.png'),
     'mirtilo':require('../assets/mirtilo.png'),
     'suculenta':require('../assets/suculenta.png'),
-    'plant2':require('../assets/planta2.png')
+    'morango':require('../assets/morango.png'),
+    'tomate':require('../assets/tomate.png'),
+    'coentro':require('../assets/coentro.png'),
+    'hortela':require('../assets/hortela.png'),
+    'alface':require('../assets/alface.png'),
+    'cebola':require('../assets/cebola.png'),
+
 }
--- src/global/plants.js
+++ src/global/plants.js
@@ -1,7 +1,16 @@
 import { appImages } from "./images"
 
 const plantImageById = {
-    'default':appImages['plant1']
+    'default':appImages['plant1'],
+    '1':appImages['tomate'],
+    '2':appImages['cebola'],
+    '3':appImages['alface'],
+    '4':appImages['alho'],
+    '5':appImages['morango'],
+    '6':appImages['pimenta_do_reino'],
+    '7':appImages['mirtilo'],
+    '8':appImages['coentro'],
+    '9':appImages['hortela'],
 }
 
 export function getPlantImage(id){
